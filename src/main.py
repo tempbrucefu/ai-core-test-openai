9d561183cd896e8729f11566fc36b4f5ccbe8447
Wenlong Fu <Wenlong.Fu@sap.com>
2024-02-21 10:05:31 +1300

    test envir

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 from flask import Flask, request, jsonify
 import openai
+import logging
+import os
 
 openai.api_type = "azure"
 openai.api_version = "2023-05-15" #"2022-12-01"
 
 app = Flask(__name__)
+testVcap = os.getenv("VCAP_SERVICES","not setting")
+logging.info(f"vcap = {testVcap}")
 
 @app.route("/v2/completion", methods=["POST"])
 def predict_completion():
