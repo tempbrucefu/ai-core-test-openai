bd3fb37e401e1f87dd42964841ccd10c9e29e7d6
Wenlong Fu <Wenlong.Fu@sap.com>
2024-02-21 15:13:25 +1300

    remove not used

--- src/main.py
+++ src/main.py
@@ -6,11 +6,6 @@ openai.api_type = "azure"
 openai.api_version = "2023-05-15" #"2022-12-01"
 
 app = Flask(__name__)
-testVcap = os.getenv("VCAP_SERVICES","not setting")
-try:
-    app.logger.info(f"vcap = {testVcap}")
-except Exception as e:
-    app.logger.info(f"vcap error {e}")
 
 @app.route("/v2/completion", methods=["POST"])
 def predict_completion():
@@ -42,14 +37,14 @@ def predict_chat_completion():
 @app.route("/v2/vcap", methods=["GET"])
 def get_vcap():
     try:
-        testVcap2 = os.getenv("vcap","not setting")
+        testVcap = os.getenv("VCAP_SERVICES","not setting")
         try:
             import json
-            print(f"vcap = {testVcap2}")
-            testVcap2Obj = json.loads(str(testVcap2))
+            print(f"vcap = {testVcap}")
+            testVcap2Obj = json.loads(str(testVcap))
             print(f"vcap = {testVcap2Obj}")
         except Exception as e:
             print(f"vcap error {e}")
-        return jsonify({"error": str(testVcap2Obj)})
+        return jsonify({"VCAP_SERVICES": str(testVcap2Obj)})
     except Exception as e:
         return jsonify({"error": str(e)})
